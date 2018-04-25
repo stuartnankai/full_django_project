40ccbb875dd7c973ea0826f782da42880e6f49e0
Jiansun <jians@kth.se>
2018-04-25 13:49:09 +0200

    Add models for courses

--- courses/models.py
+++ courses/models.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 
-
 from django.db import models
 
 
@@ -16,8 +15,39 @@ class Course(models.Model):
     fav_num = models.IntegerField(default=0, verbose_name="savenumber")
     image = models.ImageField(upload_to="courses/%Y/%m", verbose_name="figure", max_length=100)
     click_num = models.IntegerField(default=0, verbose_name="clicknum")
-    add_time = models.DateTimeField(default=datetime.now,verbose_name="addtime")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="addtime")
 
     class Meta:
         verbose_name = "courseinfo"
+        verbose_name_plural = verbose_name
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, verbose_name="lesson")
+    name = models.CharField(max_length=100, verbose_name="chaptername")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="addtime")
+
+    class Meta:
+        verbose_name = "lesson"
+        verbose_name_plural = verbose_name
+
+
+class Video(models.Model):
+    lesson = models.ForeignKey(Lesson, verbose_name="video")
+    name = models.CharField(max_length=100, verbose_name="videoname")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="addtime")
+
+    class Meta:
+        verbose_name = "video"
+        verbose_name_plural = verbose_name
+
+
+class CourseResource(models.Model):
+    course = models.ForeignKey(Course,verbose_name="courseresource")
+    name = models.CharField(max_length=100, verbose_name="sourcername")
+    download = models.FileField(max_length=100,upload_to="course/%Y/%m",verbose_name="resource")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="addtime")
+
+    class Meta:
+        verbose_name = "courseresource"
         verbose_name_plural = verbose_name
